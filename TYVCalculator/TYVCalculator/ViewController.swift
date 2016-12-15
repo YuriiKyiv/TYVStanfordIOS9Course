4c2e131d314baa857ed29e7af81a243376c01454
Yurii <yurii.trokhymchuk@idapgroup.com>
2016-12-15 22:40:52 +0200

    create project

---
+++ TYVCalculator/TYVCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TYVCalculator
+//
+//  Created by Юрій on 15.12.16.
+//  Copyright © 2016 Yurii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
